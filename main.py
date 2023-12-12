bd2367e18128cd8bac0ca0ee8f10133aadcebb41
Glucti <vanjatg@gmail.com>
2023-12-12 15:31:03 +0100

    finished

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from turtle import Screen, Turtle
 import random
 
-DEGREES = [90, 180, 270, 0]
+DEGREES = [180, 270, 90, 0]
 
 screen = Screen()
 screen.setup(1000, 1000)
+screen.colormode(255)
 
 # rand color generator
+def rand_color_generator():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+turtle = Turtle()
+turtle.pensize(20)
+turtle.speed(20)
+
+for i in range(500):
+    turtle.color(rand_color_generator())
+    turtle.forward(35)
+    turtle.setheading(random.choice(DEGREES))
+
+
+
 
