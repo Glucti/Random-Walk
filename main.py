570ccaa8f5daf0167cb34bba954e3303cc0595f2
Glucti <vanjatg@gmail.com>
2023-12-12 15:34:53 +0100

    reformated:

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ screen = Screen()
 screen.setup(1000, 1000)
 screen.colormode(255)
 
+
 # rand color generator
 def rand_color_generator():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return r, g, b
+
 
 turtle = Turtle()
 turtle.pensize(20)
@@ -22,7 +24,3 @@ for i in range(500):
     turtle.color(rand_color_generator())
     turtle.forward(35)
     turtle.setheading(random.choice(DEGREES))
-
-
-
-
